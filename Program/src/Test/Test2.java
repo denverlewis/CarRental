e59e51b7547ca272ae2166bbb3ca6d264770c4e5
Paddy <paddyc22@hotmail.co.uk>
2019-11-28 12:24:34 +0000

    Changed two lines

--- Program/src/Test/Test2.java
+++ Program/src/Test/Test2.java
@@ -4,5 +4,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("Hello how are you");
         System.out.println("Happy Days");
+        System.out.println("cool");
     }
 }
