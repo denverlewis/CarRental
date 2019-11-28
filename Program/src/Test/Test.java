921cee5c2a7ef3de33f9f9f4bdd74c1544030652
dlewis525 <HaK,pM(#-z68Xa*;/H>
2019-11-28 11:56:03 +0000

    Initial commit

---
+++ Program/src/Test/Test.java
@@ -0,0 +1,12 @@
+package Test;
+
+/**
+ * Created by Denver Lewis - B00530157
+ * Created on 28/11/2019
+ * A Program
+ */
+public class Test {
+	 public static void main(String[] args) {
+		  System.out.println("Hello");
+	 }
+}
